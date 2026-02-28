7b6a3238d7b90644d49ba53de212ab1f4cdca6a2
Dvolk28 <dylanmvolk@gmail.com>
2026-02-28 15:19:00 -0500

    Update Skyline.tsx

--- client/src/components/examples/Skyline.tsx
+++ client/src/components/examples/Skyline.tsx
@@ -5,7 +5,7 @@ export default function SkylineExample() {
   return (
     <TooltipProvider>
       <div className="h-[450px]" style={{ background: "linear-gradient(to bottom, #070b14, #0a0f1a)" }}>
-        <Skyline litCount={120} totalCount={5000} />
+        <Skyline lights={[]} onLightClick={() => {}} />
       </div>
     </TooltipProvider>
   );
